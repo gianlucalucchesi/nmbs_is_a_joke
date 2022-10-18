3f4318e839f4b52322c7f8f42fc2d62324bb6efe
Gianluca Lucchesi <gianluca@Gianlucas-MacBook-Pro.local>
2022-10-18 22:38:37 +0200

    Spring boot application

---
+++ src/main/java/com/gianlucalucchesi/nmbs_is_a_joke/NmbsIsAJokeApplication.java
@@ -0,0 +1,13 @@
+package com.gianlucalucchesi.nmbs_is_a_joke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmbsIsAJokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NmbsIsAJokeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gianlucalucchesi/nmbs_is_a_joke/NmbsIsAJokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gianlucalucchesi.nmbs_is_a_joke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmbsIsAJokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
