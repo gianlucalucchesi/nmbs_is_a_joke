6af9aefff3109fb1ad81dcac37a4836077c095b5
Gianluca Lucchesi <gianluca@Gianlucas-MacBook-Pro.local>
2022-10-30 20:05:03 +0100

    Added comments about Twitter API

--- src/main/java/com/nmbs_is_a_joke/api/helper/TwitterHelper.java
+++ src/main/java/com/nmbs_is_a_joke/api/helper/TwitterHelper.java
@@ -4,6 +4,17 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
+/**
+ * twitter4j.properties file needed in resources folder with following properties:
+ * oauth.consumerKey=
+ * oauth.consumerSecret=
+ * oauth.accessToken=
+ * oauth.accessTokenSecret=
+ * ==> can be retrieved from Twitter Developer Platform
+ * https://www.youtube.com/watch?v=yROTjD7Tl-0
+ * https://www.youtube.com/watch?v=Ak37Ri06faY
+ *
+ */
 public class TwitterHelper {
 
     public void postTweet(String tweet) throws TwitterException {
