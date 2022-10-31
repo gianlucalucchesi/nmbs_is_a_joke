f40cb4daa2e4574d0ef0d2922266ffae8a5b8f46
Gianluca Lucchesi <gianluca@Gianlucas-MacBook-Pro.local>
2022-10-31 21:54:36 +0100

    Refactoring our of main class

--- src/main/java/com/nmbs_is_a_joke/api/NmbsIsAJokeApplication.java
+++ src/main/java/com/nmbs_is_a_joke/api/NmbsIsAJokeApplication.java
@@ -1,37 +1,18 @@
 package com.nmbs_is_a_joke.api;
 
-import com.nmbs_is_a_joke.api.helper.TwitterHelper;
-import com.nmbs_is_a_joke.api.service.IRailService;
-import com.nmbs_is_a_joke.api.service.TwitterService;
+import com.nmbs_is_a_joke.api.service.NmbsIsAJokeService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import twitter4j.TwitterException;
 
 import java.io.IOException;
-import java.util.Calendar;
 
 @SpringBootApplication
 public class NmbsIsAJokeApplication {
 
     public static void main(String[] args) throws IOException, TwitterException {
         SpringApplication.run(NmbsIsAJokeApplication.class, args).close();
-
-        IRailService iRailService = new IRailService();
-        TwitterService twitterService = new TwitterService();
-        TwitterHelper twitterHelper = new TwitterHelper();
-
-        System.out.printf("START: %s%n", System.currentTimeMillis());
-        Calendar today = Calendar.getInstance();
-
-        int totalDelayInSeconds = iRailService.getTotalDelayInSecondsForGivenDay(today);
-        int totalDelayedTrains = iRailService.getTotalDelayedTrains();
-        int totalCancelledJourneys = iRailService.getTotalTrainsCancelled();
-        int totalJourneys = iRailService.getVehicleList().size();
-
-        String tweetBody = twitterService.getTweetBody(today, totalJourneys, totalDelayedTrains, totalCancelledJourneys, totalDelayInSeconds);
-        twitterHelper.postTweet(tweetBody);
-
-        System.out.printf("STOP: %s%n", System.currentTimeMillis());
+        NmbsIsAJokeService.runNmbsIsAJokeApplication();
     }
 
 }
---
+++ src/main/java/com/nmbs_is_a_joke/api/service/NmbsIsAJokeService.java
@@ -0,0 +1,32 @@
+package com.nmbs_is_a_joke.api.service;
+
+import com.nmbs_is_a_joke.api.helper.TwitterHelper;
+import org.springframework.stereotype.Service;
+import twitter4j.TwitterException;
+
+import java.io.IOException;
+import java.util.Calendar;
+
+@Service
+public class NmbsIsAJokeService {
+
+    public static void runNmbsIsAJokeApplication() throws IOException, TwitterException {
+        IRailService iRailService = new IRailService();
+        TwitterService twitterService = new TwitterService();
+        TwitterHelper twitterHelper = new TwitterHelper();
+
+        System.out.printf("START: %s%n", System.currentTimeMillis());
+        Calendar today = Calendar.getInstance();
+
+        int totalDelayInSeconds = iRailService.getTotalDelayInSecondsForGivenDay(today);
+        int totalDelayedTrains = iRailService.getTotalDelayedTrains();
+        int totalCancelledJourneys = iRailService.getTotalTrainsCancelled();
+        int totalJourneys = iRailService.getVehicleList().size();
+
+        String tweetBody = twitterService.getTweetBody(today, totalJourneys, totalDelayedTrains, totalCancelledJourneys, totalDelayInSeconds);
+        twitterHelper.postTweet(tweetBody);
+
+        System.out.printf("STOP: %s%n", System.currentTimeMillis());
+    }
+
+}
