ab3af69b1d62cecc03602c0606961de0dcd11f1f
Gianluca Lucchesi <gianluca@Gianlucas-MacBook-Pro.local>
2022-10-31 22:14:39 +0100

    Added config for war package

--- src/main/java/com/nmbs_is_a_joke/api/NmbsIsAJokeApplication.java
+++ src/main/java/com/nmbs_is_a_joke/api/NmbsIsAJokeApplication.java
@@ -2,12 +2,24 @@ package com.nmbs_is_a_joke.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * War file necessary to deploy on server
+ * https://javapointers.com/spring/spring-boot/create-war-file-in-spring-boot/
+ * run 'mvn package' to build
+ */
 @SpringBootApplication
-public class NmbsIsAJokeApplication {
+public class NmbsIsAJokeApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(NmbsIsAJokeApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(NmbsIsAJokeApplication.class);
     }
 
 }
